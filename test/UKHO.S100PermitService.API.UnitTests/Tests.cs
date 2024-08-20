bb13b7ea26c4416434896e4bc0fd8a3a6d6d70a5
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-08-20 15:23:16 +0530

    17025 - updated unit test project & added one testcase to check in pipeline. AB#17025

---
+++ test/UKHO.S100PermitService.API.UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UKHO.S100PermitService.API.UnitTests
+{
+    [TestFixture(typeof(int))]
+    [TestFixture(typeof(string))]
+    public class Tests<T>
+    {
+        [Test]
+        public void TestType()
+        {
+            Assert.Pass($"The generic test type is {typeof(T)}");
+        }
+    }
+}
