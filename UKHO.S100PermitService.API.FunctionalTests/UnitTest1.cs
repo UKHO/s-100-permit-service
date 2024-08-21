3a1379bdadcd6c34f88e01e1987938020cf0344c
Arit Sarkar <arit14975@mastek.com>
2024-08-21 18:13:42 +0530

    AB#171174- Added automation test project

---
+++ UKHO.S100PermitService.API.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UKHO.S100PermitService.API.FunctionalTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
