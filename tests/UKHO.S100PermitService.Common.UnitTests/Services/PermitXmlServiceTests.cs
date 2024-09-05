ab77b68ede3a8f3d37708975f91f5db7c389576f
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-09-05 12:31:03 +0530

    173676 - Added Fluent Assertion in Unit Test AB#173676

--- tests/UKHO.S100PermitService.Common.UnitTests/Services/PermitXmlServiceTests.cs
+++ tests/UKHO.S100PermitService.Common.UnitTests/Services/PermitXmlServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using UKHO.S100PermitService.Common.Models;
+﻿using FluentAssertions;
+using UKHO.S100PermitService.Common.Models;
 using UKHO.S100PermitService.Common.Services;
 
 namespace UKHO.S100PermitService.Common.UnitTests.Services
@@ -32,11 +33,11 @@ namespace UKHO.S100PermitService.Common.UnitTests.Services
             };
             var result = _fakePermitXmlService.MapPermit(DateTimeOffset.Parse("2024-09-02+01:00"), "fakeDataServerIdentifier", "fakeDataServerName", "fakeUserPermit", 1, new List<products>());
 
-            Assert.That(result.header.dataServerName, Is.EqualTo(expectedResult.header.dataServerName));
-            Assert.That(result.header.dataServerIdentifier, Is.EqualTo(expectedResult.header.dataServerIdentifier));
-            Assert.That(result.header.issueDate, Is.EqualTo(expectedResult.header.issueDate));
-            Assert.That(result.header.userpermit, Is.EqualTo(expectedResult.header.userpermit));
-            Assert.That(result.header.version, Is.EqualTo(expectedResult.header.version));
+            result.header.dataServerName.Should().Be(expectedResult.header.dataServerName);
+            result.header.dataServerIdentifier.Should().Be(expectedResult.header.dataServerIdentifier);
+            result.header.issueDate.Should().Be(expectedResult.header.issueDate);
+            result.header.userpermit.Should().Be(expectedResult.header.userpermit);
+            result.header.version.Should().Be(expectedResult.header.version);
         }
 
         [Test]
@@ -74,12 +75,12 @@ namespace UKHO.S100PermitService.Common.UnitTests.Services
             };
             var result = _fakePermitXmlService.MapPermit(DateTimeOffset.Parse("2024-09-02+05:30"), "fakeDataServerIdentifier", "fakeDataServerName", "fakeUserPermit", 1, products);
 
-            Assert.That(result.products[0].id, Is.EqualTo(expectedResult.products[0].id));
-            Assert.That(result.products[0].datasetPermit[0].issueDate, Is.EqualTo(expectedResult.products[0].datasetPermit[0].issueDate));
-            Assert.That(result.products[0].datasetPermit[0].editionNumber, Is.EqualTo(expectedResult.products[0].datasetPermit[0].editionNumber));
-            Assert.That(result.products[0].datasetPermit[0].encryptedKey, Is.EqualTo(expectedResult.products[0].datasetPermit[0].encryptedKey));
-            Assert.That(result.products[0].datasetPermit[0].expiry, Is.EqualTo(expectedResult.products[0].datasetPermit[0].expiry));
-            Assert.That(result.products[0].datasetPermit[0].filename, Is.EqualTo(expectedResult.products[0].datasetPermit[0].filename));
+            result.products[0].id.Should().Be(expectedResult.products[0].id);
+            result.products[0].datasetPermit[0].issueDate.Should().Be(expectedResult.products[0].datasetPermit[0].issueDate);
+            result.products[0].datasetPermit[0].editionNumber.Should().Be(expectedResult.products[0].datasetPermit[0].editionNumber);
+            result.products[0].datasetPermit[0].encryptedKey.Should().Be(expectedResult.products[0].datasetPermit[0].encryptedKey);
+            result.products[0].datasetPermit[0].expiry.Should().Be(expectedResult.products[0].datasetPermit[0].expiry);
+            result.products[0].datasetPermit[0].filename.Should().Be(expectedResult.products[0].datasetPermit[0].filename);
         }
     }
 }
