10f086fdd4761b33e40f3e1ef30b278ad7c2da23
Shailendra Shinde <shailen14599@mastek.com>
2024-09-11 18:00:46 +0530

    172721 - added holdings config class AB#172721 AB#176646

---
+++ src/UKHO.S100PermitService.Common/Configuration/HoldingsServiceApiConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace UKHO.S100PermitService.Common.Configuration
+{
+    public class HoldingsServiceApiConfiguration : IHoldingsServiceApiConfiguration
+    {
+        public string HoldingsClientId { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ src/UKHO.S100PermitService.Common/Configuration/IHoldingsServiceApiConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace UKHO.S100PermitService.Common.Configuration
+{
+    public interface IHoldingsServiceApiConfiguration
+    {
+        public string HoldingsClientId { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
