e95378c5676bdc86e254c2a18e75d869daf46ec8
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-09-16 18:43:26 +0530

    updated base controller

--- src/UKHO.S100PermitService.API/Controllers/BaseController.cs
+++ src/UKHO.S100PermitService.API/Controllers/BaseController.cs
@@ -6,7 +6,6 @@ namespace UKHO.S100PermitService.API.Controllers
     public abstract class BaseController<T> : ControllerBase
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-        protected new HttpContext HttpContext => _httpContextAccessor.HttpContext!;
 
         protected BaseController(IHttpContextAccessor httpContextAccessor)
         {
