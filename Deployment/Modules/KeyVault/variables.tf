47cd435e1649a5a68f5ac79c8862399fd0e9447d
Vinod Nerella <vinod1502506@mastek.com>
2024-10-01 01:37:29 +0530

    declared few variables

--- Deployment/Modules/KeyVault/variables.tf
+++ Deployment/Modules/KeyVault/variables.tf
@@ -32,4 +32,53 @@ variable "tags" {
 variable "name_midkv" {
   
 }
+#############
+
+variable "resource_group_name" {
+  type    = string
+ }
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "hub_subscription_id" {
+  type = string
+}
+
+variable "sku_name" {
+  type = map(any)
+  default = {
+            "dev"     =  "P1v2"            
+            "vni"     =  "P1v3"
+            "iat"     =  "P1v3"            
+            "e2e"     =  "P1v3"
+            "qa"      =  "P1v3"
+            live      =  "P1v3"
+            }
+}
+
+variable "spoke_rg" {
+  type = string
+}
+
+variable "pe_rg" {
+  type = string
+}
+
+variable "spoke_vnet_name" {
+  type = string
+}
+
+variable "spoke_subnet_name" {
+  type = string
+}
+
+variable "pe_vnet_name" {
+  type = string
+}
+
+variable "pe_subnet_name" {
+  type = string
+}
 
--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -16,3 +16,14 @@ terraform {
 provider "azurerm" {
   features {}
 }
+provider "azurerm" {
+  features {} 
+  alias = "hub"
+  subscription_id = var.hub_subscription_id
+}
+
+provider "azurerm" {
+  features {} 
+  alias = "erp"
+  subscription_id = var.subscription_id
+}
