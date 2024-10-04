ce98cc8a689c1024b128fb8185cfde745165f5b4
Shailendra Shinde <shailen14599@mastek.com>
2024-10-04 10:28:22 +0530

    176372 - Updated unit tests AB#176372

--- src/UKHO.S100PermitService.Common/Handlers/WaitAndRetryPolicy.cs
+++ src/UKHO.S100PermitService.Common/Handlers/WaitAndRetryPolicy.cs
@@ -26,7 +26,7 @@ namespace UKHO.S100PermitService.Common.Handlers
                              res.StatusCode == HttpStatusCode.TooManyRequests).WaitAndRetry(retryCount, _ => TimeSpan.FromSeconds(sleepDuration),
                              onRetry: (response, timespan, retryAttempt, context) =>
                              {
-                                 var correlationId = response.Result.RequestMessage!.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant() == "x-correlation-id");
+                                 var correlationId = response.Result.RequestMessage!.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant() == PermitServiceConstants.XCorrelationIdHeaderKey);
                                  var retryAfter = 0;
                                  logger
                                  .LogInformation(eventId.ToEventId(), "Re-trying service request for Uri: {RequestUri} with delay: {delay}ms and retry attempt {retry} with _X-Correlation-ID:{correlationId} as previous request was responded with {StatusCode}.",
