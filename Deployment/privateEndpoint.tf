1b8963fcda7e260d07cb256f567f1edcf7d8a680
Surendra Behara <surendr11609@mastek.com>
2024-10-09 15:05:15 +0530

    provider updated

--- Deployment/privateEndpoint.tf
+++ Deployment/privateEndpoint.tf
@@ -1,21 +1,21 @@
 data "azurerm_resource_group" "rg" {
-    provider = azurerm.erp
+    provider = azurerm.ps
     name = var.pe_rg
 } 
 
 data "azurerm_resource_group" "perg" {
-    provider = azurerm.erp
+    provider = azurerm.ps
     name = var.spoke_rg
 }
 
 data "azurerm_virtual_network" "pevn" {
-    provider = azurerm.erp
+    provider = azurerm.ps
     name = var.pe_vnet_name
     resource_group_name = var.spoke_rg
 }
 
 data "azurerm_subnet" "pesn" {
-    provider = azurerm.erp
+    provider = azurerm.ps
     name = var.pe_subnet_name
     virtual_network_name = var.pe_vnet_name
     resource_group_name = var.spoke_rg
