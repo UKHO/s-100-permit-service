e153cc3b6f476b839ff0f6a17f07dd5d91840176
Chaitali Shidid <Chaital14503@Mastek.Com>
2024-10-22 14:54:44 +0530

    181294 - stub changes AB#181294

--- src/UKHO.S100PermitService.StubService/Stubs/UserPermitsServiceStub.cs
+++ src/UKHO.S100PermitService.StubService/Stubs/UserPermitsServiceStub.cs
@@ -60,7 +60,7 @@ namespace UKHO.S100PermitService.StubService.Stubs
             string filePath;
             switch(licenceId)
             {
-                case int n when (n >= 1 && n <= 12) || (n == 50):
+                case int n when (n >= 1 && n <= 13) || (n == 50):
                     filePath = Path.Combine(_responseFileDirectoryPath, $"response-200-licenceid-{licenceId}.json");
                     responseMessage.StatusCode = HttpStatusCode.OK;
                     break;
