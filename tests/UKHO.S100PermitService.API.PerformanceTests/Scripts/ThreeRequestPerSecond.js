52d92f7a754e65aa4817158a9c8a2bf4a9ff45bb
rushikeshkhopatkar <rushike12794@mastek.com>
2025-05-07 12:46:43 +0530

    AB#219041 - Added performance scripts for baselining

---
+++ tests/UKHO.S100PermitService.API.PerformanceTests/Scripts/ThreeRequestPerSecond.js
@@ -0,0 +1,43 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+const Config = JSON.parse(open('./../config.json'));
+const payload = JSON.parse(open('./../Data/Payload.json')); 
+
+export const options = {
+  scenarios: {
+    ThreeRequestPerSecond: {
+      executor: 'constant-arrival-rate',
+      rate: 3, // 3 iterations (requests) per second
+      timeUnit: '1s', // per second
+      duration: '10m', // run for 10 minute
+      preAllocatedVUs: 5, // initial VUs to allocate
+      maxVUs: 10, // maximum VUs to allow
+    },
+  },
+};
+export default function ThreeRequestPerSecond() {
+    
+    let url = Config.Url;
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${Config.Token}`,
+        },
+    };  
+    
+    const res = http.post(url, JSON.stringify(payload), params);
+    console.log(res.status);
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+}
+export function handleSummary(data) {
+    return {
+        ["./../Summary/ThreeRequestPerSecond" + new Date().toISOString().substr(0, 19).replace(/(:|-)/g, "").replace("T", "_") + ".html"]: htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+        ["./../Summary/ThreeRequestPerSecond" + new Date().toISOString().substr(0, 19).replace(/(:|-)/g, "").replace("T", "_") + ".json"]: JSON.stringify(data),
+    }
+}
