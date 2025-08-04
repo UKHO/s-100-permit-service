a3b1f278c76995c6efc8ac7cdf6ad9d871f53bdf
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-08-04 10:39:47 +0100

    clean up

--- Deployment/privateEndpoint.tf
+++ Deployment/privateEndpoint.tf
@@ -20,21 +20,3 @@ data "azurerm_subnet" "pesn" {
     virtual_network_name = var.pe_vnet_name
     resource_group_name = var.spoke_rg
 }
-
-module "private_endpoint_link" {
-  source              = "github.com/UKHO/tfmodule-azure-private-endpoint-private-link?ref=0.6.0"
-  providers = {
-    azurerm.hub   = azurerm.hub
-    azurerm.spoke   = azurerm.ps
-  }
-  vnet_link           = local.vnet_link
-  private_connection  = [local.private_connection]
-  zone_group          = local.zone_group 
-  pe_identity         = [local.pe_identity]
-  pe_environment      = local.env_name 
-  pe_vnet_rg          = var.spoke_rg 
-  pe_vnet_name        = var.pe_vnet_name
-  pe_subnet_name      = var.pe_subnet_name
-  pe_resource_group   = data.azurerm_resource_group.rg
-  dns_resource_group  = local.dns_resource_group
-}
