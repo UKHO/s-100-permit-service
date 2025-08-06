38ca30e873471c340b9b63526b104e09e94cb21c
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-08-06 11:11:54 +0100

    Update privateEndpoint.tf

--- Deployment/privateEndpoint.tf
+++ Deployment/privateEndpoint.tf
@@ -1,8 +1,3 @@
-data "azurerm_resource_group" "rg" {
-    provider = azurerm.ps
-    name = var.pe_rg
-} 
-
 data "azurerm_resource_group" "perg" {
     provider = azurerm.ps
     name = var.spoke_rg
@@ -35,6 +30,6 @@ module "private_endpoint_link" {
   pe_vnet_rg          = var.spoke_rg 
   pe_vnet_name        = var.pe_vnet_name
   pe_subnet_name      = var.pe_subnet_name
-  pe_resource_group   = data.azurerm_resource_group.rg
+  pe_resource_group   = azurerm_resource_group.rg
   dns_resource_group  = local.dns_resource_group
 }
