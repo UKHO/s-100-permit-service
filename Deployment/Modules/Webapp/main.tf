34146615633e4e5998efa01f553c2f8d9e357b23
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2025-12-08 10:27:17 +0000

    Remove public network access from staging slot
    Updated the `azurerm_windows_web_app_slot` resource for the "staging" slot by removing the `public_network_access_enabled` property. The `tags` property remains unchanged, indicating that the staging slot will no longer have the public network access setting.

--- Deployment/Modules/Webapp/main.tf
+++ Deployment/Modules/Webapp/main.tf
@@ -40,8 +40,7 @@ resource "azurerm_windows_web_app" "webapp_service" {
 resource "azurerm_windows_web_app_slot" "staging" {
   name                          = "staging"
   app_service_id                = azurerm_windows_web_app.webapp_service.id
-  tags                          = azurerm_windows_web_app.webapp_service.tags 
-  public_network_access_enabled = false
+  tags                          = azurerm_windows_web_app.webapp_service.tags   
     
   site_config {
     application_stack {    
